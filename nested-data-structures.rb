2dcbe4f1fb4160b310e6679c40f4d10110789f98
Jon Wenger <jonwenger783@gmail.com>
2018-06-08 23:18:55 +0000

    automatically backed up by learn

---
+++ nested-data-structures.rb
@@ -0,0 +1,11 @@
+#CREATE A HASH WITH NESTED ARRAYS OF YOUR GROUPS FAVORITE SONGS 
+
+#CREATE A HASH 
+
+group_name = {
+  :person_1 => ["fav song 1". "fav song 2", ...],
+} 
+
+#call the FIRST persons SECOND FAVORITE song
+
+#call the SECOND persons FIRST FAVORITE song
